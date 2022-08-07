53f45664b1f42e205dc5d7ad71cd10c60fef8999
eugeneshine1 <eugeneshine1@gmail.com>
2022-08-07 16:00:11 -0400

    done

--- message-service.js
+++ message-service.js
@@ -1,36 +1,52 @@
 export default class MessageService {
+    getAllMessages() {
+        const request = new XMLHttpRequest();
 
-   getAllMessages() {
-       const request = new XMLHttpRequest();
+        return new Promise(function (resolve, reject) {
+            // Setup our listener to process completed requests
+            request.onload = function () {
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    const threads = JSON.parse(request.responseText);
+                    // this data is passed to the success callback
+                    resolve(threads);
+                } else {
+                    // this data is passed to the failure callback
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
 
-       new Promise(function (resolve, reject) {
-           // Setup our listener to process completed requests
-           request.onload = function () {
-               // Process the response
-               if (request.status >= 200 && request.status < 300) {
-                   const threads = JSON.parse(request.responseText); // 'This is the returned text.'
-                   resolve(threads);
-               } else {
-                   reject({
-                       status: request.status,
-                       statusText: request.statusText
-                   });
-               }
-           };
+            request.open("GET", "http://zipcode.rocks:8085/messages");
 
-           request.open("GET", "http://zipcode.rocks:8085/messages");
+            request.send();
+        })
+    }
 
-           request.send();
-       }).then(successCallback, errorCallback);
+    createNewMessage(message) {
+        const request = new XMLHttpRequest();
 
-      function successCallback(response) {
-             // This data comes from the resolve method
-             console.log(response);
-      }
-      function errorCallback(response) {
-             // This data comes from the reject method
-             console.log(response);
-      }
-   }
+        return new Promise(function (resolve, reject) {
+            // Setup our listener to process completed requests
+            request.onload = function () {
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    resolve(JSON.parse(request.responseText));
+                } else {
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
 
+            request.open("POST", "http://zipcode.rocks:8085/ids/${message.fromid}/messages");
+
+            request.send(JSON.stringify(message));
+        });
+    }
 }
